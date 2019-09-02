aea76b4dc244ca65969c65334466defad2076546
ziyixu <ziyixu@ziyixu-pc.wireless-1x.unc.edu>
2019-09-02 01:47:51 -0400

    Novice 1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,37 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int customers = scan.nextInt() ;
+		System.out.println(customers);
+		
+		for(int i=1;i<=customers;i++)
+		{
+			String firstname = scan.next();
+			System.out.println(firstname);
+			
+			String lastname = scan.next();
+			System.out.println(lastname);
+			
+			int items = scan.nextInt();
+			System.out.println(items);
+			
+			int quantity = scan.nextInt();
+			System.out.println(quantity);
+			
+			String name = scan.next();
+			System.out.println(name);
+			
+			double price = scan.nextDouble();
+			System.out.println(price);
+			
+			char firstletter = firstname.charAt(1);
+			
+			double netprice = quantity*price;
+			
+			System.out.println(firstletter+". "+lastname+": "+netprice);
+						
+		}
+		
 		
 	}
 }
